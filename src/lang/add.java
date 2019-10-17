2da2e78a4733eecaa9016e6aa57497d96fead621
SivaranjaniJB <sivaranjani.jabas25@gmail.com>
2019-10-17 10:26:18 +0530

    first

---
+++ src/lang/add.java
@@ -0,0 +1,17 @@
+package lang;
+
+public class add {
+	public void hardware()
+	{
+		System.out.println("hi");
+	}
+	public void software() {
+		System.out.println("hello");
+	}
+	public static void main(String[] args) {
+		
+		
+	}
+
+
+}
